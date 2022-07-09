6deedfdaf07c9cb1ab05f5ff1a2dbfe653207183
mavidon <marcusotuvictor@gmail.com>
2022-07-09 09:46:32 -0700

    Checking Uppercase

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int main ()
-
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	char c;
-
-	for (c = 'A' ; c <= 'Z' ; c++) {
-		putchar (c);
-	}
-
-	return(0);
+	return (write(1, &c, 1));
 }
