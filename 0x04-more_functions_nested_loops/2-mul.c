ce986979026378401350815679527ae20848562d
mavidon <marcusotuvictor@gmail.com>
2022-07-09 13:01:21 -0700

    Multiplier updated

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,11 @@
 
 /**
  * mul - multiplier function
- * @m: int to multiply
- * @n: int to multiply
+ * @a: int to multiply
+ * @b: int to multiply
  * Return: result of the two multiplied values
  */
-int mul(int a, int b)
+int mul(int a, int b);
 {
 	return (a * b);
 }
